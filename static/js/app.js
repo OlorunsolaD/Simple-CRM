a47c96f57fcebb60751272922a3d3f2e85ddbe01
OlorunsolaD <daramolaolorunsola@gmail.com>
2024-04-24 17:23:15 +0100

    First project

---
+++ static/js/app.js
@@ -0,0 +1,26 @@
+
+
+// Message/Notification Timer
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+
+{
+
+
+    message_timeout.style.display = "none";
+
+}, 5000);
+
+
+
+
+
+
+
+
+
+
+
+
